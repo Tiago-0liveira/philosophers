65e32780fc5949bc2b253c051c7fb24830dd7db2
Tiago-0liveira <tfgoliveira@gmail.com>
2023-12-04 23:51:19 +0000

    create some types just to begin with

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:03:14 by tiagoliv          #+#    #+#             */
-/*   Updated: 2023/12/03 20:44:27 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2023/12/04 23:49:22 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,28 @@
 # define USAGE "Usage: ./philo <n_philo> <time_to_die> \
 <time_to_eat> <time_to_sleep> [<number_of_times_each_philosopher_must_eat>]"
 
-typedef struct s_philo {
+enum e_philo_state {
+	THINKING,
+	EATING,
+	SLEEPING,
+	DEAD
+};
 
+typedef struct s_philo {
+	pthread_t		thread_id;
+	size_t			eat_count;
+	size_t			action_time_elapsed;
+	e_philo_state	state;
+	pthread_mutex_t	*philo_mutex;
 }	t_philo;
 
+typedef struct s_table {
+	size_t		n_philo;
+	size_t		time_to_die;
+	size_t		time_to_eat;
+	size_t		time_to_sleep;
+	t_philo		*philos;
+
+}	t_table;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:04:32 by tiagoliv          #+#    #+#             */
-/*   Updated: 2023/12/03 20:33:18 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2023/12/04 23:50:53 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int	main(int argc, char *argv[])
 {
-
+	check_args(argc, argv);
 
 	return (0);
 }
 
-bool	check_args(int argc, char*argv[])
+bool	check_args(int argc, char *argv[])
 {
-
-
+	if (argc < 4 || argc > 5)
+	{
+		printf("%s\n", USAGE);
+		return (false);
+	}
 }
