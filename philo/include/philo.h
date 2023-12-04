1574f567481a6f16d8d59576ce10229d1156c3eb
Tiago-0liveira <tfgoliveira@gmail.com>
2023-12-04 01:04:06 +0000

    new repo

---
+++ philo/include/philo.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 20:03:14 by tiagoliv          #+#    #+#             */
+/*   Updated: 2023/12/03 20:44:27 by tiagoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <pthread.h>
+# include <stdbool.h>
+
+# define USAGE "Usage: ./philo <n_philo> <time_to_die> \
+<time_to_eat> <time_to_sleep> [<number_of_times_each_philosopher_must_eat>]"
+
+typedef struct s_philo {
+
+}	t_philo;
+
+
+#endif
