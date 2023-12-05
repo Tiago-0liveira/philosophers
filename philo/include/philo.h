1f1a3811551fe389bb825280407d17bcb13ecece
Tiago Filipe Oliveira <tiagoliv@c1r16s9.42lisboa.com>
2023-12-05 15:55:13 +0000

    time handling funcs

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:03:14 by tiagoliv          #+#    #+#             */
-/*   Updated: 2023/12/04 23:49:22 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2023/12/05 15:47:04 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <pthread.h>
 # include <stdbool.h>
+# include <sys/time.h>
 
 # define USAGE "Usage: ./philo <n_philo> <time_to_die> \
 <time_to_eat> <time_to_sleep> [<number_of_times_each_philosopher_must_eat>]"
@@ -28,20 +29,23 @@ enum e_philo_state {
 };
 
 typedef struct s_philo {
-	pthread_t		thread_id;
-	size_t			eat_count;
-	size_t			action_time_elapsed;
-	e_philo_state	state;
-	pthread_mutex_t	*philo_mutex;
+	pthread_t			thread_id;
+	size_t				eat_count;
+	size_t				action_time_elapsed;
+	enum e_philo_state	state;
+	pthread_mutex_t		*philo_mutex;
 }	t_philo;
 
 typedef struct s_table {
-	size_t		n_philo;
-	size_t		time_to_die;
-	size_t		time_to_eat;
-	size_t		time_to_sleep;
-	t_philo		*philos;
-
+	size_t				n_philo;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	size_t				start_millis;
+	t_philo				*philos;
 }	t_table;
+
+size_t	get_milis(struct timeval *tv);
+void	milisleep(size_t	milis);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 20:04:32 by tiagoliv          #+#    #+#             */
-/*   Updated: 2023/12/04 23:50:53 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2023/12/05 15:47:26 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,39 @@
 
 int	main(int argc, char *argv[])
 {
-	check_args(argc, argv);
-
+	//check_args(argc, argv);
+	(void)argc;
+	(void)argv;
+	milisleep(1000);
 	return (0);
+}
+
+size_t	get_milis(struct timeval *tv)
+{
+	return (tv->tv_sec * 1000 + tv->tv_usec / 1000);
+}
+
+void	milisleep(size_t	milis)
+{
+	struct timeval	beg;
+	struct timeval	end;
+
+	gettimeofday(&beg, NULL);
+	while (1)
+	{
+		gettimeofday(&end, NULL);
+		if ((get_milis(&end) - get_milis(&beg)) >= milis)
+			return ;
+	}
 }
 
 bool	check_args(int argc, char *argv[])
 {
+	(void)argv;
 	if (argc < 4 || argc > 5)
 	{
 		printf("%s\n", USAGE);
 		return (false);
 	}
+	return (true);
 }
